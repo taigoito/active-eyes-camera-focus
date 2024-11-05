b6fb3bef490cd9925e94ccd42cee9ee8759e3db3
Taigo Ito <webmaster@qwel.design>
2024-11-05 11:51:43 +0900

    Restricts mobile operation

--- photos.js
+++ photos.js
@@ -6,14 +6,21 @@
 
 class Photos {
   constructor() {
-    document.querySelectorAll('.photo').forEach(elem => {
+    const photos = document.querySelectorAll('.photo');
+    if (!photos || !photos.length) return;
+
+    photos.forEach(elem => {
       elem.addEventListener('mousemove', (event) => {
-        const rect = elem.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
         const image = elem.querySelector('.photo__image');
         image.style.transform = '';
-        image.style.transformOrigin = `${x}px ${y}px`;
+        if ('ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0) {
+          image.style.transformOrigin = `50% 50%`;
+        } else {
+          const rect = elem.getBoundingClientRect();
+          const x = event.clientX - rect.left;
+          const y = event.clientY - rect.top;
+          image.style.transformOrigin = `${x}px ${y}px`;
+        }
       });
 
       elem.addEventListener('mouseleave', () => {
